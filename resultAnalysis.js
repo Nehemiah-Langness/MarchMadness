43fa7d9b4db97c2a6078b3c97e6436e3dafabe85
Nehemiah-Langness <32148982+Nehemiah-Langness@users.noreply.github.com>
2018-02-25 18:55:35 -0600

    Styling
    Update styling;
    Added Search filter;
    Added more team meta data

--- resultAnalysis.js
+++ resultAnalysis.js
@@ -124,57 +124,63 @@ var htmlHelper = (function(){
         return "<img src='" + team.icon + "' alt='logo' class='team-logo'>"
     }
 
+    var renderText = function(text){
+        var safe = $('<div>').text(text).html().replace("'", "&#39;").replace('"', "&#34;");
+        return safe;
+    }
+
     var renderHeader = function (round){
         return "<span class='h3'>Round "+ (round.round + 1)  +"</span><hr/>";
     }
 
     var renderPopover = function(team){
         return "data-toggle='popover' "+
-            "title='" + team.team + "' "+
+            "title='" + renderText(team.team) + "' "+
             "data-placement='bottom' "+
             "data-trigger='hover' "+
             "data-html='true' "+
             "data-content='"+
                 "<img src=\"" + team.icon + "\" height=\"60px\"><br/>" +
                 "<b>Rank</b>: " + team.rank + "<br/>" +
-                "<b>Location</b>: " + team.location + "<br/>"+ 
-                "<b>Teamname</b>: " + team.nickname + "<br/>"+ 
+                "<b>Location</b>: " + renderText(team.location) + "<br/>"+ 
+                "<b>Teamname</b>: " + renderText(team.nickname) + "<br/>"+ 
             "'";
     }
 
-    var renderTeam = function(team){
-        return "<div class='team' style='" + (team.color? "color: " + team.color + ";": "" ) +"'>"+
-                    image(team) + 
-                    " <span " + renderPopover(team) + ">" +
-                        (team.nickname ? team.nickname : team.team) + "</span>" +
-                "</div>"
+    var renderTeam = function(team, winner){
+        return "<div class='clearfix match-result' style='" + (team.color? "background-color: " + team.color + ";": "" ) +"' " + renderPopover(team) + ">"+
+                    "<div class='float-left'>" +
+                        "<div class='team' style='" + (team.color? "color: #fff;": "" ) +"'>"+
+                            image(team) + 
+                            " " + (team.nickname ? renderText(team.nickname) : renderText(team.team)) +
+                        "</div>" +
+                    "</div>"+
+                    (winner.team == team.team ?
+                    "<div class='float-right'>"+ 
+                            fa.new("trophy") +
+                    "</div>" : "") + 
+                "</div>";
     };
 
     var match = function (matchResults){
-        var team1Display = (matchResults.team1.nickname ? matchResults.team1.nickname : matchResults.team1.team);
-        var team2Display = (matchResults.team2.nickname ? matchResults.team2.nickname : matchResults.team2.team);
+        var team1 = matchResults.team1;
+        var team2 = matchResults.team2;
 
-        var top = team1Display.length < team2Display.length ? matchResults.team1 : matchResults.team2;
-        var bottom = team1Display.length < team2Display.length ? matchResults.team2 : matchResults.team1;
+        var team1Display = (team1.nickname ? team1.nickname : team1.team);
+        var team2Display = (team2.nickname ? team2.nickname : team2.team);
 
-        return "<div class='match'>" + 
-                    "<div class='clearfix'>"+
-                        "<div class='float-left'>" +
-                            renderTeam(top) +
-                        "</div>"+
-                        "<div class='float-right'"+ 
-                        " <span " + renderPopover(matchResults.winner) + ">" +
-                                fa.new("trophy") + " " + image(matchResults.winner) +
-                            "</span>" +
-                        "</div>" + 
-                    "</div>" +
+        var top = team1Display.length < team2Display.length ? team1 : team2;
+        var bottom = team1Display.length < team2Display.length ? team2 : team1;
+
+        return "<div class='match' data-teams='" + renderText(team1.team).toLowerCase() + ":" + renderText(team2.team).toLowerCase() + ":" + renderText(team1.nickname).toLowerCase() + ":" + renderText(team2.nickname).toLowerCase() + "'>" + 
+                    renderTeam(top, matchResults.winner) +
                     "<hr/>" +
-                    renderTeam(bottom) +
+                    renderTeam(bottom, matchResults.winner) +
                 "</div>";
     }
 
     var renderWinner = function(team) {
-        return "<div class='text-center'><div class='winner'><span class='h2'>" + fa.new("trophy") + " <img src='" + team.icon +"' alt='logo' class='team-logo'> </br>" + team.team + "</span></div></div><br/>";
+        return "<div class='text-center'><div class='winner' style='"+ (team.color ? "background-color:" + team.color + "; color: #fff;" : "") +"'><span class='h2'>" + fa.new("trophy") + " <img src='" + team.icon +"' alt='logo' class='team-logo'> </br>" + renderText(team.team) + "</span></div></div><br/>";
     }
 
     return {
