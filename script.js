7785da1b2a5f2daa9b3abd133e66b6449d56a2d3
Nehemiah-Langness <32148982+Nehemiah-Langness@users.noreply.github.com>
2018-03-07 20:42:51 -0600

    Info
    Added Percentage to each round;
    Added Collapse to each round;
    Added Upset notifier;

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 fa = (function () {
-    var icon = function(className) {
-        return "<i class='fas fa-" + className +"'></i>";
+    var icon = function (className) {
+        return "<i class='fas fa-" + className + "'></i>";
     }
 
-    var $icon = function(className) {
+    var $icon = function (className) {
         return $(icon(className));
     }
 
@@ -15,21 +15,21 @@ fa = (function () {
 
 var linq = (function () {
 
-    var max = function(array){
+    var max = function (array) {
         return array.reduce(function (current, next) {
             if (!current || next > current) return next;
             return current;
         });
     };
 
-    var min = function(array){
+    var min = function (array) {
         return array.reduce(function (current, next) {
             if (!current || next < current) return next;
             return current;
         });
     };
 
-    var selectMany = function(array, selectFunction) {
+    var selectMany = function (array, selectFunction) {
         var list = [];
         array.forEach(function (item) {
             selectFunction(item).forEach(function (innerItem) {
@@ -39,24 +39,27 @@ var linq = (function () {
         return list;
     }
 
-    var mode = function(array){
+    var mode = function (array) {
         var items = {};
         var modeItem = array[0];
         var max = 1;
-        array.forEach(function (item){
-            if(items[item] == null)
+        array.forEach(function (item) {
+            if (items[item] == null)
                 items[item] = 1;
             else
-                items[item]++;  
+                items[item]++;
 
-            if(items[item] > max)
-            {
+            if (items[item] > max) {
                 modeItem = item;
                 max = items[item];
             }
         });
-        
-        return modeItem;
+
+        return {
+            item: modeItem,
+            count: array.length,
+            occurances: max
+        };
     }
 
     return {
@@ -67,31 +70,18 @@ var linq = (function () {
     }
 }());
 
-var Reflection = function(item){
-    var properties = [];
-
-    for(var key in item)
-        if (item.hasOwnProperty(key))
-            properties.push({
-                key: key, 
-                value: item[key]
-            });
-
-    return properties;
-}
-
-Array.prototype.max = function() {
+Array.prototype.max = function () {
     return linq.max(this);
-}; 
+};
 
-Array.prototype.min = function() {
+Array.prototype.min = function () {
     return linq.min(this);
-}; 
+};
 
-Array.prototype.mode = function() {
+Array.prototype.mode = function () {
     return linq.mode(this);
 };
 
-Array.prototype.selectMany = function(selectFunction) {
+Array.prototype.selectMany = function (selectFunction) {
     return linq.selectMany(this, selectFunction);
 }; 
